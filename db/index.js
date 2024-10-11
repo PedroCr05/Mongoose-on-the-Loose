47f8d3349aa53598c734669ccde2f53e3d1dae86
pedrocruz <JayJMarquez05@gmail.com>
2024-10-11 12:50:14 -0500

    Files and folders created | Mongoose DB boilerplate created

---
+++ db/index.js
@@ -0,0 +1,29 @@
+const mongoose = require(`mongoose`);
+
+mongoose
+  .connect(
+    `mongodb+srv://PedroCr05:--[password]--@clusters.k4z0f.mongodb.net/movie-projects?retryWrites=true&w=majority&appName=clusters`
+  )
+  .then(() => {
+    console.log(`
+        =================================
+        +=[Connection Success: MongoDB]+=
+        =================================
+        `);
+  })
+  .catch((e) => {
+    console.error(
+      `
+        ===============================
+        =+=+=+ERROR SERVER CRASH]=+=+=+
+        ===============================        
+        `,
+      e.message
+    );
+  });
+
+mongoose.set(`debug`, true);
+
+const db = mongoose.connection;
+
+module.exports = db;
